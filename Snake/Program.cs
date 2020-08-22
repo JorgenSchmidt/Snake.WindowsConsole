81ddc3dc92dd3c94c16f68a931349e723c4fdf31
Joe_897 <jo3smith.joe@yandex.ru>
2020-08-22 16:39:17 +0300

    L_02

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -3,7 +3,22 @@
 namespace Snake {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            int x1 = 7;
+            int y1 = 8;
+            char sym1 = '#';
+            Console.SetCursorPosition(x1,y1);
+            Console.WriteLine(sym1);
+
+            Console.ReadLine();
+
+            int x2 = 3;
+            int y2 = 4;
+            char sym2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.WriteLine(sym2);
+
+            Console.ReadLine();
         }
     }
 }
