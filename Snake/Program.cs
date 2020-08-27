60c924e2f641cef90c9423444e7ea1fa41355f10
Joe_897 <jo3smith.joe@yandex.ru>
2020-08-27 21:18:35 +0300

    L_09

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,13 +15,13 @@ namespace Snake {
             length = 100;
             speed = 100;
 
-            Horisontal hLine1 = new Horisontal(0, length, 0, '*');
+            Horisontal hLine1 = new Horisontal(0, length, 0, '+');
             hLine1.getLine();
-            Horisontal hLine2 = new Horisontal(0, length, height, '*');
+            Horisontal hLine2 = new Horisontal(0, length, height, '+');
             hLine2.getLine();
-            Vertical vLine1 = new Vertical(0, 0, height, '*');
+            Vertical vLine1 = new Vertical(0, 0, height, '+');
             vLine1.getLine();
-            Vertical vLine2 = new Vertical(length, 0, height, '*');
+            Vertical vLine2 = new Vertical(length, 0, height, '+');
             vLine2.getLine();
 
             Point p = new Point(2, 2, '*');
