1a837497ed8a7a05c79d3e6d88f7c72c0a58075f
Joe_897 <jo3smith.joe@yandex.ru>
2020-08-31 12:02:53 +0300

    L_13

--- Snake/GameOver.cs
+++ Snake/GameOver.cs
@@ -8,8 +8,8 @@ namespace Snake
     {
 
         public static void gameOver(int _length, int _height) {
-            Console.SetCursorPosition(_length / 2 - 4, _height / 2); Console.WriteLine("GameOver");
-            Console.SetCursorPosition(_length / 2 - 13, (_height / 2) + 2); Console.WriteLine("Press any key for continue");
+            Console.SetCursorPosition(_length / 2 - 4, (_height / 2) - 1); Console.WriteLine("GameOver");
+            Console.SetCursorPosition(_length / 2 - 13, (_height / 2) + 1); Console.WriteLine("Press any key for continue");
             Console.ReadLine();
             Console.SetCursorPosition(_length, _height + 3);
         }
--- Snake/Wall.cs
+++ Snake/Wall.cs
@@ -7,19 +7,22 @@ namespace Snake
     class Wall{
 
         List<Figure> wallList;
-
+        private int sleepWall1;
+        private int sleepWall2;
         public Wall(int _length, int _height) {
 
             wallList = new List<Figure>();
+            sleepWall1 = 6;
+            sleepWall2 = 12;
 
             Horisontal hLine1 = new Horisontal(0, _length, 0, '+');
-            hLine1.getLine(10);
+            hLine1.getLine(sleepWall1);
             Horisontal hLine2 = new Horisontal(0, _length, _height, '+');
-            hLine2.getLine(10);
+            hLine2.getLine(sleepWall1);
             Vertical vLine1 = new Vertical(0, 0, _height, '+');
-            vLine1.getLine(10);
+            vLine1.getLine(sleepWall2);
             Vertical vLine2 = new Vertical(_length, 0, _height, '+');
-            vLine2.getLine(10);
+            vLine2.getLine(sleepWall2);
 
             wallList.Add(hLine1);
             wallList.Add(hLine2);
