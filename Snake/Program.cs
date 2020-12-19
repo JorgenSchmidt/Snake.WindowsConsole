f7db04abe05e095debb2258873d05f5032698bf1
Joe_897 <jo3smith.joe@yandex.ru>
2020-12-19 21:58:31 +0500

    Prepare the basis for the game with two snakes

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,45 +13,57 @@ namespace Snake {
 
         static void Main(string[] args) {
 
+            firstMode();
+
+        }
+
+        private static void secondMode() {
+
             height = 21;
             length = 75;
             speed = 80;
             changeSpeedAfterEat = 3;
-            
 
-            Wall wall = new Wall(length, height) ;
 
-            Point p = new Point(2, 2, '*');
-            Snake s = new Snake(p, 4, Direction.right);
-            s.getLine();
+            Wall wall = new Wall(length, height);
+
+            Point p1 = new Point(2, 2, '*');
+            Snake s1 = new Snake(p1, 4, Direction.right);
+            s1.getLine();
 
             SpawnFood foodspawner = new SpawnFood(length, height, '$');
             Point food = foodspawner.foodSP();
             food.getPoint();
 
-            while (true) {
+            while (true)
+            {
 
-                if (wall.isHit(s) || s.isHitTail()) {
+                if (wall.isHit(s1) || s1.isHitTail())
+                {
                     break;
                 }
 
-                if (s.eat(food)) {
+                if (s1.eat(food))
+                {
                     food = foodspawner.foodSP();
                     food.getPoint();
-                    if (speed > 10 ) { speed -= changeSpeedAfterEat; }
+                    if (speed > 10) { speed -= changeSpeedAfterEat; }
                     GetInformationPanel.inputTheGameInformation(length, height);
-                } else {
-                    s.toDir();
+                }
+                else
+                {
+                    s1.toDir();
                 }
 
                 Thread.Sleep(speed);
 
-                if (Console.KeyAvailable) {
+                if (Console.KeyAvailable)
+                {
                     ConsoleKeyInfo key = Console.ReadKey();
-                    s.handleKey(key.Key);
+                    s1.handleKey(key.Key);
 
                     //Осторожно, костыль!!!!!
-                    for (int i = 0; i<= height; i++)
+                    for (int i = 0; i <= height; i++)
                     {
                         Point.drawTheNotIdentificatedPoint(0, i, '+');
                     }
@@ -62,5 +74,64 @@ namespace Snake {
             GameOver.gameOver(length, height);
 
         }
+
+        private static void firstMode () {
+
+            height = 21;
+            length = 75;
+            speed = 80;
+            changeSpeedAfterEat = 3;
+
+
+            Wall wall = new Wall(length, height);
+
+            Point p = new Point(2, 2, '*');
+            Snake s = new Snake(p, 4, Direction.right);
+            s.getLine();
+
+            SpawnFood foodspawner = new SpawnFood(length, height, '$');
+            Point food = foodspawner.foodSP();
+            food.getPoint();
+
+            while (true)
+            {
+
+                if (wall.isHit(s) || s.isHitTail())
+                {
+                    break;
+                }
+
+                if (s.eat(food))
+                {
+                    food = foodspawner.foodSP();
+                    food.getPoint();
+                    if (speed > 10) { speed -= changeSpeedAfterEat; }
+                    GetInformationPanel.inputTheGameInformation(length, height);
+                }
+                else
+                {
+                    s.toDir();
+                }
+
+                Thread.Sleep(speed);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    s.handleKey(key.Key);
+
+                    //Осторожно, костыль!!!!!
+                    for (int i = 0; i <= height; i++)
+                    {
+                        Point.drawTheNotIdentificatedPoint(0, i, '+');
+                    }
+
+                }
+            }
+
+            GameOver.gameOver(length, height);
+
+        }
+
     } 
 } 
