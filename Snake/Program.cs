692342222a52dd7e79fa78fc537fb6936a24d5ed
Joe_897 <jo3smith.joe@yandex.ru>
2020-12-19 22:49:46 +0500

    Begin for add the dual-mode

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -17,6 +17,67 @@ namespace Snake {
 
         }
 
+        private static void secondDualMode()
+        {
+
+            height = 21;
+            length = 75;
+            speed = 80;
+            changeSpeedAfterEat = 3;
+
+
+            Wall wall = new Wall(length, height);
+
+            GetInformationPanel.inputTheGameInformation(length, height);
+
+            Point p = new Point(2, 2 + Wall.getStartPosition(), '*');
+            Snake s = new Snake(p, 4, Direction.right);
+            s.getLine();
+
+            SpawnFood foodspawner = new SpawnFood(length, height + 1 + Wall.getStartPosition(), '$');
+            Point food = foodspawner.foodSP();
+            food.getPoint();
+
+            while (true)
+            {
+
+                if (wall.isHit(s) || s.isHitTail())
+                {
+                    break;
+                }
+
+                if (s.eat(food))
+                {
+                    food = foodspawner.foodSP();
+                    food.getPoint();
+                    if (speed > 10) { speed -= changeSpeedAfterEat; }
+                    GetInformationPanel.inputTheGameInformation(length, height);
+                }
+                else
+                {
+                    s.toDir();
+                }
+
+                Thread.Sleep(speed);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    s.handleKey(key.Key);
+
+                    //Осторожно, костыль!!!!!
+                    for (int i = 0 + Wall.getStartPosition(); i <= height + Wall.getStartPosition(); i++)
+                    {
+                        Point.drawTheNotIdentificatedPoint(0, i, '+');
+                    }
+
+                }
+            }
+
+            GameOver.gameOver(length, height + Wall.getStartPosition());
+
+        }
+
         private static void firstMode () {
 
             height = 21;
